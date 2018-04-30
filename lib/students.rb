dde34ecf8079edd0aa8fee68869886769da19b5d
Molly N <mollykathrynnelson@gmail.com>
2018-04-30 01:26:10 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,13 +1,15 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   "SELECT MAX(gpa) as highest_gpa FROM students"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) as highest_gpa FROM students"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) as highest_gpa FROM students"
 end
 
 def total_tardies_for_all_students
